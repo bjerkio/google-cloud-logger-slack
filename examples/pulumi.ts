7eccaaa096fe5993dd69158e61f9bc6809871ba3
Simen A. W. Olsen <so@bjerk.io>
2021-09-02 18:48:49 +0200

    chore: Update example

--- examples/pulumi.ts
+++ examples/pulumi.ts
@@ -1,24 +1,24 @@
-import * as gcp from "@pulumi/gcp";
-import * as pulumi from "@pulumi/pulumi";
-import { makeHandler } from "gcl-slack";
-import { handlePubSubMessage } from "pubsub-http-handler";
+import * as gcp from '@pulumi/gcp';
+import * as pulumi from '@pulumi/pulumi';
+import { makeHandler } from 'gcl-slack';
+import { handlePubSubMessage } from 'pubsub-http-handler';
 
-const name = "slack";
+const name = 'slack';
 
 export const topic = new gcp.pubsub.Topic(name, {}, { provider });
 
 const config = new pulumi.Config(name);
 
 topic.onMessagePublished(name, {
-  region: "europe-west1",
-  runtime: "nodejs14",
+  region: 'europe-west1',
+  runtime: 'nodejs14',
   environmentVariables: {
-    WEBHOOK_URL: config.require("webhook-url"),
+    WEBHOOK_URL: config.require('webhook-url'),
   },
   callback: async (message) => {
     const handler = makeHandler({
-      type: "webhook",
-      webhookOptions: { url: process.env.WEBHOOK_URL ?? "" },
+      type: 'webhook',
+      webhookOptions: { url: process.env.WEBHOOK_URL ?? '' },
     });
 
     await handlePubSubMessage({
@@ -36,6 +36,6 @@ const logSink = new gcp.logging.ProjectSink(name, {
 
 new gcp.pubsub.TopicIAMMember(name, {
   topic: topic.name,
-  role: "roles/pubsub.publisher",
+  role: 'roles/pubsub.publisher',
   member: logSink.writerIdentity,
 });
