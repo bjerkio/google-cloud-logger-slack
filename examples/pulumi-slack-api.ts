b71b2b83db51ec018d3c3d64523d2434f4f46db1
Simen A. W. Olsen <so@bjerk.io>
2021-09-15 19:27:35 +0200

    feat: Add `makePulumiCallback` (#5)
    Add a specialized callback function to make it easer to connect with Pulumi.

---
+++ examples/pulumi-slack-api.ts
@@ -0,0 +1,29 @@
+import * as gcp from '@pulumi/gcp';
+import * as pulumi from '@pulumi/pulumi';
+import { makePulumiCallback } from 'gcl-slack';
+
+const name = 'slack';
+
+export const topic = new gcp.pubsub.Topic(name, {}, { provider });
+
+const config = new pulumi.Config(name);
+
+topic.onMessagePublished(name, {
+  region: 'europe-west1',
+  runtime: 'nodejs14',
+  environmentVariables: {
+    SLACK_TOKEN: config.require('token'),
+  },
+  callback: makePulumiCallback('api'),
+});
+
+const logSink = new gcp.logging.ProjectSink(name, {
+  filter: 'operation.producer="github.com/bjerkio/nestjs-slack@v1"',
+  destination: pulumi.interpolate`pubsub.googleapis.com/${topic.id}`,
+});
+
+new gcp.pubsub.TopicIAMMember(name, {
+  topic: topic.name,
+  role: 'roles/pubsub.publisher',
+  member: logSink.writerIdentity,
+});
