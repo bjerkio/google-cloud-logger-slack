ae3de73dfa7eeee98c1f61cf90973add3deafe35
Simen A. W. Olsen <so@bjerk.io>
2023-04-30 14:07:15 +0200

    refactor!: Drop axios for undici/fetch
    BREAKING CHANGE: Dropping axios in favor of undici might break tests. For example, if using nock. See https://github.com/nock/nock/issues/2183

--- src/methods/webhook.ts
+++ src/methods/webhook.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import { fetch } from 'undici';
 import { parseLog } from '../parse-log';
 import type { LogEntry, SlackMethod, SlackWebhookOptions } from '../types';
 
@@ -7,18 +7,25 @@ export type WebhookResponse = 'ok' | string;
 export class SlackWebhookMethod implements SlackMethod {
   constructor(private readonly config: SlackWebhookOptions) {}
 
-  async send(entry: LogEntry): Promise<void> {
+  async send(entry: LogEntry): Promise<{ success: boolean }> {
     const parsedLog = await parseLog(entry);
     await this.sendRequest(parsedLog);
+
+    return { success: true };
   }
 
-  private async sendRequest(data: unknown): Promise<void> {
-    await axios.post(this.config.url, data).catch((error: AxiosError) => {
-      if (error.response) {
-        throw new Error(
-          `Could not send request to Slack Webhook: ${error.response.data}`,
-        );
-      }
+  private async sendRequest(data: unknown): Promise<{ success: true }> {
+    const result = await fetch(this.config.url, {
+      method: 'POST',
+      body: JSON.stringify(data),
     });
+
+    if (result.status !== 200) {
+      throw new Error(
+        `Could not send request to Slack Webhook: ${await result.text()}`,
+      );
+    }
+
+    return { success: true };
   }
 }
