fc157ad40afb68e8af078037182f1d7d9a346fde
Simen A. W. Olsen <so@bjerk.io>
2023-04-30 14:53:08 +0200

    chore: remove test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -9,14 +9,6 @@ module.exports = {
     '.*dist.*',
   ],
   collectCoverageFrom: ['src/**/{!(fixtures),}.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
-    },
-  },
   transform: {
     '\\.[jt]sx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
