3e95f2343a56dcaea3c4cff373775459480da40e
Simen A. W. Olsen <so@bjerk.io>
2023-04-30 14:37:06 +0200

    chore: fixup jest config

--- jest.config.js
+++ jest.config.js
@@ -2,25 +2,22 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: [
+    '.*.utils.ts',
     '.*/fixtures.ts',
-    '.*/__fixtures__/*',
     '.*/*.fixtures.ts',
     '.*/dist/.*',
     '.*dist.*',
   ],
   collectCoverageFrom: ['src/**/{!(fixtures),}.ts'],
-  // TODO: Improve test coverage enough to uncomment these 👇
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 80,
-  //     functions: 80,
-  //     lines: 80,
-  //     statements: -10,
-  //   },
-  // },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
     },
+  },
+  transform: {
+    '\\.[jt]sx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
 };
